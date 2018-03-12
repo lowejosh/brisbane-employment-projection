c58cb2fad33f6cb1e6f64248663e8de564213a16
Your name <lowejosh50@gmail.com>
2018-03-12 23:04:36 +1000

    changed the theme again and added javscript to the buttons

--- index.php
+++ index.php
@@ -8,18 +8,19 @@
         <div class="header">
             <span class="header-title">Brisbane Employment Projection</span>
         </div>
-        <div class="main">
+        <div class="main" id="main">
             <span class="sub-title">Would you like to</span>
             <div class="center-wrap">
-                <div class="choice-button">
+                <div class="choice-button" onclick="choiceClicked(0)">
                     <span class="choice-text">View occupation employment projection</span>
                 </div>
-                <div class="choice-button">
+                <div class="choice-button" id="industryButton" onclick="choiceClicked(1)"> 
                     <span class="choice-text">View industry employment projection</span>
                 </div>
             </div>
         </div>
         <div class="footer">
         </div>
+    <script src="js/script.js" type="text/javascript"></script>
     </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Placeholder
+function choiceClicked(choice) {
+    if(choice == '0') {
+        document.getElementById("main").innerHTML = "test";
+    } else if (choice== '1') {
+        document.getElementById("main").innerHTML = "test";
+    } else {
+        document.write("Error: please refresh and try again"); 
+    }
+}
