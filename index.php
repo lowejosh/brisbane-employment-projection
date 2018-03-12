034ff35442173e91bf5396be9fe5b9345f6884f5
Your name <lowejosh50@gmail.com>
2018-03-12 16:58:23 +1000

    added centered buttons with vertically aligned text

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
         </div>
         <div class="main">
             <span class="sub-title">Would you like to...</span>
+            <div class="center-wrap">
+                <div class="choice-button">
+                    <span class="choice-text">View occupation employment projection</span>
+                </div>
+                <div class="choice-button">
+                    <span class="choice-text">View industry employment projection</span>
+                </div>
+            </div>
         </div>
         <div class="footer">
         </div>
