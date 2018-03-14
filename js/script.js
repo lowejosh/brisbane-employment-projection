94a07dacc05887646ae7ddec36520c6d23f60eff
Your name <lowejosh50@gmail.com>
2018-03-14 19:21:52 +1000

    progress on javascripting in the form to connect to the php database

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 function choiceClicked(choice) {
     if(choice == '0') {
-        document.getElementById("main").innerHTML = "test";
+        var output = `
+            <form>
+                <input type="hidden" value="0"></input> 
+                <span class="sub-title">Please choose your region</span>
+                
+            
+            </form>
+            `
+        document.getElementById("main").innerHTML = output;
     } else if (choice== '1') {
-        document.getElementById("main").innerHTML = "test";
+        var output = `
+            <form>
+                <input type="hidden" value="1"></input> 
+                test
+            
+            </form>
+            `
+        document.getElementById("main").innerHTML = output;
     } else {
         document.write("Error: please refresh and try again"); 
     }
