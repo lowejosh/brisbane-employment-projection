e3af931db2430d2dce6c2ae77b623b66e28eec4e
Your name <lowejosh50@gmail.com>
2018-03-14 23:00:33 +1000

    added return button and dynamically rendered html dependant on the GET values

--- index.php
+++ index.php
@@ -9,19 +9,15 @@
             <span class="header-title">Brisbane Employment Projection</span>
         </div>
         <div class="main" id="main">
-            <span class="sub-title">Would you like to</span>
-            <div class="center-wrap">
-                <form method="get">
-                    <button type="submit" name="action" class="choice-button" value="Occupation">
-                        View occupation employment projection
-                    </button>
-                    <button type="submit" name="action" class="choice-button" value="Industry"> 
-                        View industry employment projection
-                    </button>
-                </form>
-            </div>
+        <?php 
+            if(!isset($_GET['action'])) {
+                include 'includes/data-choice.php';
+            }
+        ?>
+        <a class="return-button" href="index.php">Return</a>
         </div>
         <div class="footer">
+            Joshua Lowe | Github
         </div>
     <script src="js/script.js" type="text/javascript"></script>
     </body>
