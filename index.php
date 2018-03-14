8cbcd6689f78fcf92726e4917f8420006da1e553
Your name <lowejosh50@gmail.com>
2018-03-14 23:59:49 +1000

    added github link and a link style

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
             ?>
         </div>
         <div class="footer">
-            Joshua Lowe | Github
+            Joshua Lowe | <a class="link" href="https://github.com/lowejosh/brisbane-employment-projection">Github</a>
         </div>
     </body>
 </html>
