c03ca6e863c1fab504573ada95ebe8137d718b9e
Your name <lowejosh50@gmail.com>
2018-03-15 14:35:57 +1000

    added link in the navbar to initial page

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             <span class="header-title">Brisbane Employment Projection</span>
         </div>
         <div class="navigation"> 
-            <span href="" id="data-selection">Data Selection</span> >
+            <span href="" id="data-selection"><a class="link" href="index.php">Data Selection</a></span> >
             <span href="" id="location-selection">Location</span> >
             <span href="" id="specific-selection">Occupation/Industry</span>        
         </div>
--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function setNavbar(currSel) {
+    // If it is data selection
     if (currSel == 0) {
+        // Sets the current "page" as bold and the rest with low opacity
         document.getElementById("data-selection").style.fontWeight = "bold";
         document.getElementById("location-selection").style.opacity = "0.5";
         document.getElementById("specific-selection").style.opacity = "0.5";
+        
+        // Removes the <a> tag around the text as it is not necessary
+        document.getElementById("data-selection").innerHTML = "Data Selection";
     } else {
+        
         document.getElementById("data-selection").style.fontWeight = "normal";
         document.getElementById("location-selection").style.fontWeight = "bold";
         document.getElementById("specific-selection").style.opacity = "0.5";
