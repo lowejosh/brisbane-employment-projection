438dc9f4396d1a8d0f9f788e0d00c20202a75f80
Your name <lowejosh50@gmail.com>
2018-03-17 23:51:58 +1000

    begun redesigning the layout to remove whitespace

--- includes/data-choice.php
+++ includes/data-choice.php
@@ -1,13 +1,7 @@
-<div class="vertically-centered">
-<span class="sub-title">Would you like to</span>
-    <div class="center-wrap">
-        <form method="get">
-            <button type="submit" style="background-color:#62929F;" name="action" class="choice-button" value="Occupation">
-                View occupation employment projection
-            </button>
-            <button type="submit" style="background-color:#F4EEA9;" name="action" class="choice-button" value="Industry"> 
-                View industry employment projection
-            </button>
-        </form>
-    </div>
-</div>
+<form method="get">
+    <button type="submit" style="background-color:#62929F;" name="action" class="choice-button" value="Occupation">
+        View occupation employment projection
+    </button><button type="submit" style="background-color:#F4EEA9;" name="action" class="choice-button" value="Industry"> 
+        View industry employment projection
+    </button>
+</form>
