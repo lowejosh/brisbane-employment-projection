8ae1bad755cc582ad3a9e56fa1b271e999f4374d
Your name <lowejosh50@gmail.com>
2018-03-18 19:11:37 +1000

    repositioned and styled text inside buttons and added img directory

--- includes/data-choice.php
+++ includes/data-choice.php
@@ -1,7 +1,7 @@
 <form method="get">
     <button type="submit" style="background-color:#62929F;" name="action" class="choice-button" value="Occupation">
-        View occupation employment projection
+        <span class="button-title">View occupation employment projection</span>
     </button><button type="submit" style="background-color:#F4EEA9;" name="action" class="choice-button" value="Industry"> 
-        View industry employment projection
+        <span class="button-title">View industry employment projection</span>
     </button>
 </form>
