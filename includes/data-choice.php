faa240edd3919317c1d908cd20ed91c6bd22d354
Your name <lowejosh50@gmail.com>
2018-03-18 21:38:19 +1000

    added choose buttons

--- includes/data-choice.php
+++ includes/data-choice.php
@@ -2,8 +2,10 @@
     <button type="submit" style="background-color:#62929F;" name="action" class="choice-button" value="Occupation">
         <span class="button-title">View Occupation Employment Projection</span>
         <img src="img/occupation-icon.png" class="button-icon"></img>
+        <div class="button-choose">Choose</div>
     </button><button type="submit" style="background-color:#F4EEA9;" name="action" class="choice-button" value="Industry"> 
         <span class="button-title">View Industry Employment Projection</span>
         <img src="img/industry-icon.png" class="button-icon"></img>
+        <div class="button-choose">Choose</div>
     </button>
 </form>
