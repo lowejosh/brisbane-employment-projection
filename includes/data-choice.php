39b82375bf05c36b9e2bb126e8ed4c7230b67c83
Your name <lowejosh50@gmail.com>
2018-03-18 21:21:38 +1000

    restyled button title

--- includes/data-choice.php
+++ includes/data-choice.php
@@ -1,9 +1,9 @@
 <form method="get">
     <button type="submit" style="background-color:#62929F;" name="action" class="choice-button" value="Occupation">
-        <span class="button-title">View occupation employment projection</span>
+        <span class="button-title">View Occupation Employment Projection</span>
         <img src="img/occupation-icon.png" class="button-icon"></img>
     </button><button type="submit" style="background-color:#F4EEA9;" name="action" class="choice-button" value="Industry"> 
-        <span class="button-title">View industry employment projection</span>
+        <span class="button-title">View Industry Employment Projection</span>
         <img src="img/industry-icon.png" class="button-icon"></img>
     </button>
 </form>
